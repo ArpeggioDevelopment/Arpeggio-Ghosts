2e061e0205760abefdb1cf8cd849408eb232660d
p2tch <231753395+p2tch@users.noreply.github.com>
2026-05-05 17:40:13 +0200

    feat: add plugin bootstrap (main class + plugin.yml)

---
+++ plugin/src/main/java/xyz/thehiddenobject/arpeggioghosts/plugin/bootstrap/PluginBootstrap.java
@@ -0,0 +1,15 @@
+package xyz.thehiddenobject.arpeggioghosts.plugin.bootstrap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginBootstrap extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
